da2e66e0a14094da43c5202b88fefb66ee1288cf
AdiletEngineering <adilet.joldoshbek@gmail.com>
2021-07-28 12:06:52 +0600

    created ProductSerializer

--- discount_app/views.py
+++ discount_app/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-from .models import *
-
+from .models import Product
+from .serializers import ProductSerializer
 
 @api_view(['get', 'post'])
 def product_list(request):
     if request.method == 'get':
         products = Product.objects.all()
-        serializer =
-
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
 

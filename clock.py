9b8f0425a05e3b9bff3f2905301fb91d16cd1841
AdiletEngineering <adilet.joldoshbek@gmail.com>
2021-08-09 21:46:12 +0600

    TEST

--- clock.py
+++ clock.py
@@ -8,15 +8,16 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    coupons = Coupon.objects.all()
-    print('*' * 200)
-    for coupon in coupons:
-        if coupon.deadline < timezone.now:
-            coupon.status = "EXPIRED"
-            coupon.save()
+    print('hello')
+    # coupons = Coupon.objects.all()
+    # print('*' * 200)
+    # for coupon in coupons:
+    #     if coupon.deadline < timezone.now:
+    #         coupon.status = "EXPIRED"
+    #         coupon.save()
 
-@sched.scheduled_job('interval', minutes=25)
-def timed_job():
-    r = requests.get('https://discount-adilet.herokuapp.com/')
+# @sched.scheduled_job('interval', minutes=25)
+# def timed_job():
+#     r = requests.get('https://discount-adilet.herokuapp.com/')
 
 sched.start()
